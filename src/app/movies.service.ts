80ac22d0823cb5e63f203ba9ff110a9c3b58e9d4
jbaima <jkbaima@gmail.com>
2017-10-26 07:51:45 -0500

    Don't pull all of RxJs and cleanup

--- src/app/movies.service.ts
+++ src/app/movies.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Jsonp, URLSearchParams } from '@angular/http';
-import 'rxjs/Rx';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class MoviesService {
